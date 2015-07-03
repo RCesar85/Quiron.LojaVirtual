ed7cd0261561012a4b13ab86d0b5df5b2474fd85
RCesar85 <romulocesarsd@hotmail.com>
2015-07-03 13:34:18 -0300

    Referenciando o Entity Framework

--- Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Quiron.LojaVirtual.Dominio.Entidades
 {
-    class Produto
+    public class Produto
     {
 
-        #region Propriedades
-            public int IdProduto { get; set; }
+        //#region Propriedades
+            [Key] 
+            public int ProdutoId { get; set; }
             public string Nome { get; set; }
             public string Descricao { get; set; }
             public decimal Preco { get; set; }
             public string Categoria { get; set; }
-        #endregion
+        //#endregion
     }
 }
